e2612ae357b4b475ff8dc945e5edfe9782f04cd4
harsh5052311 <sahil12211921@gmail.com>
2026-01-30 13:01:05 +0530

    second try

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def animate_text(label, text):
         time.sleep(0.1)
 
 root = tk.Tk()
-root.title("Message Display, Hola Amigo")
+root.title("Message Display, Hola Amigo1")
 root.geometry("400x200")
 
 label = tk.Label(root, text="", font=("Helvetica", 20, "bold"))
